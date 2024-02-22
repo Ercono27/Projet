6c5c0f8abbbc885f2183e05366b332e609640f04
Ercono27 <seraphin.terlinck@condorcet.be>
2024-02-22 20:54:46 +0100

    Version 0.1

--- src/Sport/Course.java
+++ src/Sport/Course.java
@@ -1,128 +1,299 @@
 package Sport;
 
 import java.math.BigDecimal;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Classe métier de gestion d'une course
+ * @author Séraphin Terlinck
+ * @version 0.1
+ */
 public class Course {
+    /**
+     * Identifiant unique de la course
+     */
     private int idCourse;
+    /**
+     * Nom de la course
+     */
     private String nom;
+    /**
+     * Cagnotte total de la course
+     */
     private BigDecimal priceMoney;
+    /**
+     * Kilométrage de la course
+     */
     private int km;
+    /**
+     * Liste des infos de la course
+     */
     private List<Infos> listeInfo = new ArrayList<>();
+    /**
+     * Liste des classements de la course
+     */
     private List<Classement> listeClassement = new ArrayList<>();
 
+    /**
+     * Constructeur par défaut de la classe Course
+     */
     public Course() {
     }
 
+    /**
+     * Constructeur paramètré de la classe Course
+     * @param idCourse identifiant unique de la course
+     * @param nom nom de la course
+     * @param priceMoney cagnotte total de la course
+     * @param km kilométrage de la course
+     * @param listeInfo liste des infos de la course
+     * @param listeClassement liste des classements de la course
+     */
+    public Course(int idCourse, String nom, BigDecimal priceMoney, int km, List<Infos> listeInfo, List<Classement> listeClassement) {
+        this.idCourse = idCourse;
+        this.nom = nom;
+        this.priceMoney = priceMoney;
+        this.km = km;
+        this.listeInfo = listeInfo;
+        this.listeClassement = listeClassement;
+    }
+
+    /**
+     * Obtient l'identifiant de la course
+     * @return l'identifiant de la course
+     */
     public int getIdCourse() {
         return idCourse;
     }
 
+    /**
+     * Définit l'identifiant de la course
+     * @param idCourse nouvel identifiant de la course
+     */
     public void setIdCourse(int idCourse) {
         this.idCourse = idCourse;
     }
 
+    /**
+     * Obtient le nom de la course
+     * @return le nom de la course
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     * Définit le nom de la course
+     * @param nom nouveau nom de la course
+     */
     public void setNom(String nom) {
         this.nom = nom;
     }
 
+    /**
+     * Obtient la récompense cagnotte totale de la course
+     * @return la récompense cagnotte totale de la course
+     */
     public BigDecimal getPriceMoney() {
         return priceMoney;
     }
 
+    /**
+     * Définit la cagnotte totale de la course.
+     * @param priceMoney nouvelle cagnotte totale de la course.
+     */
     public void setPriceMoney(BigDecimal priceMoney) {
         this.priceMoney = priceMoney;
     }
 
+    /**
+     * Obtient la distance de la course en kilomètres
+     * @return la distance de la course
+     */
     public int getKm() {
         return km;
     }
 
+    /**
+     * Définit la distance de la course en kilomètres
+     * @param km nouvelle distance de la course
+     */
     public void setKm(int km) {
         this.km = km;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Course course = (Course) o;
-        return idCourse == course.idCourse;
+    /**
+     * Obtient la liste d'informations associées à la course
+     * @return la liste d'informations associées à la course
+     */
+    public List<Infos> getListeInfo() {
+        return listeInfo;
     }
 
-    public void listeCorueursPlaceGain(){
-        //une fois course finie, affiche le coureur avec ça place et ses gains
+    /**
+     * Obtient la liste de classements associés à la course.
+     * @return la liste de classements associés à la course.
+     */
+    public List<Classement> getListeClassement() {
+        return listeClassement;
     }
-    /*
-    public BigDecimal gainTotal(){
-
-    }
-    public Coureur vainqueur(){
-        for(Classement c : listeClassement){
-            if (c.getPlace()==1)
-                return c.getCour();
+    /**
+     * Méthode pour obtenir une liste de coureurs avec leur place et leur gain
+     * @return la liste de coureurs avec leur place et leur gain
+     */
+    public List<CoureurPlaceGain> listeCoureurPlaceGain() {
+        List<CoureurPlaceGain> listeCoureursPlaceGain = new ArrayList<>();
+        int place;
+        for (Classement classement : getListeClassement()) {
+            place = classement.getPlace();
+            BigDecimal gain = classement.getGain();
+            CoureurPlaceGain cpg = new CoureurPlaceGain();
+            listeCoureursPlaceGain.add(cpg);
         }
-    }*/
-    public void addCoureur(Coureur c){
-        Classement cl=new Classement();
+        return listeCoureursPlaceGain;
+    }
+
+    /**
+     * Méthode pour calculer le gain total de la course
+     * @return le gain total de la course
+     */
+    public BigDecimal gainTotal() {
+        BigDecimal total = new BigDecimal(0);
+        for (Classement c : listeClassement) {
+            total = c.getGain().add(total);
+        }
+        return total;
+    }
+
+    /**
+     * Méthode pour obtenir le vainqueur de la course.
+     * @return le coureur vainqueur de la course.
+     */
+    public Coureur vainqueur() {
+        Coureur tmp = new Coureur();
+        for (Classement c : listeClassement) {
+            if (c.getPlace() == 1)
+                tmp = c.getCour();
+        }
+        return tmp;
+    }
+
+    /**
+     * Méthode pour ajouter un coureur à la liste de classement de la course
+     * @param c le coureur à ajouter
+     */
+    public void addCoureur(Coureur c) {
+        Classement cl = new Classement();
         cl.setCour(c);
         listeClassement.add(cl);
     }
-    public void supCoureur(Coureur c){
-        for(Classement cl : listeClassement){
-            if(cl.getCour().equals(c)){
+
+    /**
+     * Méthode pour supprimer un coureur de la liste de classement de la course
+     * @param c le coureur à supprimer
+     */
+    public void supCoureur(Coureur c) {
+        for (Classement cl : listeClassement) {
+            if (cl.getCour().equals(c)) {
                 c.setIdCoureur(-1);
                 c.setMatricule("");
                 c.setNom("");
                 c.setPrenom("");
-                Date d=new Date();
+                Date d = new Date();
                 c.setDateNaiss(d);
                 c.setNationalite("");
             }
         }
     }
-    public void resultat(Coureur c,int place, BigDecimal gain) {
-        for (Classement cl : listeClassement){
-            if(cl.getCour().equals(c)){
+
+    /**
+     * Méthode pour initialiser le classement et le gain d'un coureur.
+     * @param c le coureur à mettre à jour
+     * @param place la nouvelle place du coureur
+     * @param gain le nouveau gain du coureur
+     */
+    public void resultat(Coureur c, int place, BigDecimal gain) {
+        for (Classement cl : listeClassement) {
+            if (cl.getCour().equals(c)) {
                 cl.setPlace(place);
                 cl.setGain(gain);
             }
         }
     }
-    public void modifier(Coureur c,int place, BigDecimal gain) {
-        /*for (Classement cl : listeClassement){
-            if(cl.getCour().equals(c)){
-                cl.setPlace(place);
-                cl.setGain(gain);
-            }
-        }*/
+    /**
+     * Méthode pour modifier le classement et le gain d'un coureur déja classé
+     * @param c le coureur à modifier
+     * @param place la nouvelle place du coureur
+     * @param gain le nouveau gain du coureur
+     */
+    public void modifier(Coureur c, int place, BigDecimal gain) {
+        int x = listeClassement.indexOf(c);
+        Classement cla = new Classement();
+        if (x != -1) {
+            cla.setCour(c);
+            cla.setPlace(place);
+            cla.setGain(gain);
+            listeClassement.set(x, cla);
+        } else {
+            System.out.println("Ce coureur ne participe pas à la course.");
+        }
     }
-    public void addVille(Ville ville){
+
+    /**
+     * Méthode pour ajouter une ville aux informations associées à la course
+     * @param ville la ville à ajouter
+     */
+    public void addVille(Ville ville) {
         Infos i = new Infos();
-        Date d=new Date();
+        Date d = new Date();
         i.setVille(ville);
         i.setDepartDate(d);
         listeInfo.add(i);
     }
-    public void supVille(Ville v){
-        for(Infos i : listeInfo){
-            if(i.getVille().equals(v)){
-                v.setIdVille(-1);
-                v.setNom("");
-                v.setLatitude(-1);
-                v.setLongitude(-1);
-                v.setPays("");
-            }
+    //TODO
+    //Je ne comprends pas comment faire cette fonction avec uniquement un seul paramètre.
+       /*
+    public void modifVille(Date date2){
+        for (Infos i : listeInfo){
+            if()
         }
+    }
+    */
+
+    /**
+     * Méthode pour supprimer une ville des informations associées à la course
+     * @param v la ville à supprimer
+     */
+    public void supVille(Ville v) {
+        listeInfo.removeIf(i -> i.getVille().equals(v));
+    }
+
+    /**
+     * Méthode pour obtenir une liste de villes associées à la course
+     * @return la liste de villes associées à la course
+     */
+    public List<Ville> listeVille() {
+        List<Ville> listeVille = new ArrayList<>();
+        for (Infos i : listeInfo) {
+            listeVille.add(i.getVille());
+        }
+        return listeVille;
+    }
+
+    /**
+     * Méthode pour vérifier si le classement de la course est complet
+     * @return true si le classement est complet, false sinon
+     */
+    public boolean classementComplet() {
+        int tmp;
+        for (Classement c : listeClassement) {
+            tmp = c.getPlace();
+            if (tmp == 0)
+                return false;
+        }
+        return true;
     }
 }
--- src/Sport/Infos.java
+++ src/Sport/Infos.java
@@ -3,34 +3,97 @@ package Sport;
 import java.util.Date;
 import java.util.Objects;
 
+/**
+ * Classe métier représentant les informations associées à une ville pour une course.
+ * @author Séraphin Terlinck
+ * @version 0.1
+ */
 public class Infos {
+
+    /**
+     * Identifiant unique des informations
+     */
     private int idInfos;
+    /**
+     * Date de départ associée à la ville
+     */
     private Date departDate;
+    /**
+     * Ville associée aux informations
+     */
     private Ville ville;
-    public Infos(){
+
+    /**
+     * Constructeur par défaut de la classe Infos
+     */
+    public Infos() {
     }
 
+    /**
+     * Constructeur paramètré de la classe Infos
+     * @param idInfos Identifiant unique des informations
+     * @param departDate Date de départ associée à la ville
+     * @param ville Ville associée aux informations
+     */
+    public Infos(int idInfos, Date departDate, Ville ville) {
+        this.idInfos = idInfos;
+        this.departDate = departDate;
+        this.ville = ville;
+    }
+
+    /**
+     * Obtient l'identifiant unique des informations
+     * @return l'identifiant unique des informations
+     */
     public int getIdInfos() {
         return idInfos;
     }
 
+    /**
+     * Définit l'identifiant unique des informations
+     * @param idInfos nouvel identifiant unique des informations
+     */
     public void setIdInfos(int idInfos) {
         this.idInfos = idInfos;
     }
 
+    /**
+     * Obtient la date de départ associée à la ville
+     * @return la date de départ associée à la ville
+     */
     public Date getDepartDate() {
         return departDate;
     }
+
+    /**
+     * Définit la date de départ associée à la ville
+     * @param departDate nouvelle date de départ associée à la ville
+     */
     public void setDepartDate(Date departDate) {
         this.departDate = departDate;
     }
+
+    /**
+     * Obtient la ville associée aux informations
+     * @return la ville associée aux informations
+     */
     public Ville getVille() {
         return ville;
     }
+
+    /**
+     * Définit la ville associée aux informations
+     * @param ville nouvelle ville associée aux informations
+     */
     public void setVille(Ville ville) {
         this.ville = ville;
     }
 
+    /**
+     * Redéfinition de la méthode equals pour comparer deux ensembles d'informations par leur identifiant unique
+     * @param o l'objet à comparer
+     * @return true si les objets sont égaux, false sinon
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -38,5 +101,4 @@ public class Infos {
         Infos infos = (Infos) o;
         return idInfos == infos.idInfos;
     }
-
 }
